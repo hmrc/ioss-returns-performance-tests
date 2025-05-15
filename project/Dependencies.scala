8837571c92886606e8c55f0ede0f0315cef5038a
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-05-15 14:22:11 +0100

    VEOSS-2092 | Update dependencies for Java21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,9 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "3.6.1"
-
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.2"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.1.0"         % Test
+    "com.typesafe" % "config"                  % "1.4.3" % Test,
+    "uk.gov.hmrc" %% "performance-test-runner" % "6.1.0" % Test
   )
 
 }
